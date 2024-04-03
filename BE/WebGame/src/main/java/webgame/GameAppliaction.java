f8a437c1f15e9e53f11a40671625acbfb4be5300
GeunH <unlxrmsgur@naver.com>
2024-04-03 15:49:10 +0900

    chore : swagger 연결 및 세팅 완료 #3
    	implementation 'org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0'
    	implementation 'org.springframework.boot:spring-boot-starter-web'
    	implementation 'io.springfox:springfox-boot-starter:3.0.0'

--- BE/WebGame/src/main/java/webgame/GameAppliaction.java
+++ BE/WebGame/src/main/java/webgame/GameAppliaction.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package webgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebGameApplication {
+public class GameAppliaction {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebGameApplication.class, args);
-	}
+        SpringApplication.run(GameAppliaction.class, args);
+    }
 
 }
