157b8770a335af25f0dd043c72705e573150850d
GeunH <unlxrmsgur@naver.com>
2024-04-04 12:10:37 +0900

    feat : UserEntity 생성 #9
    UserEntity 생성
    - UserDto와 일치하도록 생성

---
+++ BE/WebGame/src/main/java/webgame/User/Entity/User.java
@@ -0,0 +1,36 @@
+package webgame.User.Entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(length = 100, unique = true) 
+    private String email;
+    
+    @Column(length = 100)
+    private String password;
+    
+    @Column(length = 50) 
+    private String provider;
+    
+    @Column(length = 50, unique = true)
+    private String nickname;
+    
+    @Column(length = 20) 
+    private LocalDate joinDate;
+    
+    @Column(length = 10) 
+    private String status;
+
+}
