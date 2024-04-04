d08fca68e50df55947eefdf68f82e7a1c2d93671
unknown <speed04313@gmail.com>
2024-04-04 16:40:14 +0900

    feat: Header 컴포넌트의 로그인 버튼 라우팅 설 #8
    헤더 컴포넌트의 로그인 버튼 클릭 시 로그인 페이지로 이동하도록 설정

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Register from '@/pages/Register';
+import Register from '@pages/Register';
+import Login from '@pages/Login';
 
 import { PAGE_URL } from '@util/constants';
 
@@ -8,7 +9,9 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path={`${PAGE_URL.HOME}`} element={<Register />} />
         <Route path={`${PAGE_URL.REGISTER}`} element={<Register />} />
+        <Route path={`${PAGE_URL.LOGIN}`} element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/Common/Header.tsx
+++ frontend/src/components/Common/Header.tsx
@@ -1,13 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
 import Button from '@components/Common/Button';
+import { PAGE_URL } from '@util/constants';
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex w-full items-center justify-between px-36 py-4">
       <p>온라인 보드게임</p>
       <Button
         text="로그인"
         onClick={() => {
-          console.log('로그인으로 이동');
+          navigate(PAGE_URL.LOGIN);
         }}
       />
     </div>
